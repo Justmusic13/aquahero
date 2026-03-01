9930a1f11ba97fd47c69f30a977e4f1cc244d12e
Justmusic13 <justin.motz@gmail.com>
2026-03-01 13:42:40 -0600

    fix: remove misplaced client points service from server

--- server/src/services/points.service.ts
+++
@@ -1,7 +0,0 @@
-import api from './api';
-import { LeaderboardEntry } from '@/types';
-
-export const getLeaderboard = async (): Promise<LeaderboardEntry[]> => {
-  const response = await api.get('/leaderboard');
-  return response.data;
-};
